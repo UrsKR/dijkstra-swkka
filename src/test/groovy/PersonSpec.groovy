a2440849f29f12ce78411e7c7d5b7fb583ba3dd9
Urs Reupke <ursreupke@gmail.com>
2011-10-16 18:49:35 +0200

    Formatting

--- src/test/groovy/PersonSpec.groovy
+++ src/test/groovy/PersonSpec.groovy
@@ -9,32 +9,32 @@ public class PersonSpec extends Specification {
 
   def "everyone knows himself"() {
     expect:
-     alex.knows alex
+      alex.knows alex
   }
 
   def "people remember their friends"() {
     when:
-     alex.befriend bettina
+      alex.befriend bettina
     then:
-     alex.knows bettina
+      alex.knows bettina
   }
 
   def "people have more than one friend"() {
     when:
-     alex.befriend bettina
-     alex.befriend carsten
+      alex.befriend bettina
+      alex.befriend carsten
     then:
-     alex.knows bettina
+      alex.knows bettina
   }
 
   def "people are linked to their friends"() {
     when:
-     alex.befriend bettina
+      alex.befriend bettina
     then:
       alex.isLinkedTo bettina
   }
 
-  def "people you know link you to other people"(){
+  def "people you know link you to other people"() {
     when:
       alex.befriend bettina
       bettina.befriend carsten
@@ -42,12 +42,12 @@ public class PersonSpec extends Specification {
       alex.isLinkedTo carsten
   }
 
-  def "a dead end does not hinder a link"(){
+  def "a dead end does not hinder a link"() {
     when:
-     alex.befriend bettina
-    bettina.befriend carsten
-    bettina.befriend doris
+      alex.befriend bettina
+      bettina.befriend carsten
+      bettina.befriend doris
     then:
-     alex.isLinkedTo doris
+      alex.isLinkedTo doris
   }
 }
