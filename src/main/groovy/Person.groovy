594ffe07939003067f646735702c5f9171a373d7
Urs Reupke <ursreupke@gmail.com>
2011-10-16 19:31:24 +0200

    Cleanup

--- src/main/groovy/Person.groovy
+++ src/main/groovy/Person.groovy
@@ -2,20 +2,20 @@ class Person {
 
   private final List<Person> friends = []
 
-  void befriend(Person person) {
+  void befriend(person) {
     friends << person
   }
 
-  boolean knows(Person person) {
+  boolean knows(person) {
     (this == person) || (friends.contains(person))
   }
 
-  boolean isLinkedTo(Person person) {
+  boolean isLinkedTo(person) {
     def checkedPersons = []
     knows(person) || isLinkedTo(person, checkedPersons)
   }
 
-  private boolean isLinkedTo(Person person, checkedPersons) {
+  private boolean isLinkedTo(person, checkedPersons) {
     if (checkedPersons.contains(this)) {
       return false
     }
@@ -23,7 +23,7 @@ class Person {
     knows(person) || hasAFriendWhoIsLinkedTo(person, checkedPersons)
   }
 
-  private def hasAFriendWhoIsLinkedTo(Person person, checkedPersons) {
+  private boolean hasAFriendWhoIsLinkedTo(person, checkedPersons) {
     friends.any {Person friend ->
       friend.isLinkedTo(person, checkedPersons)
     }
