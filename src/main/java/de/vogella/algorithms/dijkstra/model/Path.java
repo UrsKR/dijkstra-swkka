1ffcaf9299fe67a6c7fe0701f03a56f461ad5b09
Urs Reupke <ursreupke@gmail.com>
2011-10-25 18:08:31 +0200

    Paths to replace raw lists.

--- src/main/java/de/vogella/algorithms/dijkstra/model/Path.java
+++ src/main/java/de/vogella/algorithms/dijkstra/model/Path.java
@@ -1,12 +1,39 @@
 package de.vogella.algorithms.dijkstra.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
 
 public class Path {
 
   private List<Vertex> nodes;
 
+  public Path() {
+    this(new ArrayList<Vertex>());
+  }
+
+  public Path(Vertex... vertices) {
+    this(asList(vertices));
+  }
+
   public Path(List<Vertex> nodes) {
     this.nodes = nodes;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    return reflectionEquals(this, object);
+  }
+
+  @Override
+  public int hashCode() {
+    return reflectionHashCode(this);
   }
 }
---
+++ src/test/java/de/vogella/algorithms/dijkstra/model/Path_Test.java
@@ -0,0 +1,53 @@
+package de.vogella.algorithms.dijkstra.model;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Path_Test {
+
+  @Test
+  public void emptyPathsEqual() throws Exception {
+    assertThat(new Path(), is(new Path()));
+  }
+
+  @Test
+  public void identicalPathsEqual() throws Exception {
+    Path path = new Path(createVertex("1"));
+    assertThat(path, is(path));
+  }
+
+  @Test
+  public void pathsWithDifferentElementsDoNotEqual() throws Exception {
+    Path aPath = new Path(createVertex("1"));
+    Path aDifferentPath = new Path(createVertex("2"));
+    assertThat(aPath, is(not(aDifferentPath)));
+  }
+
+  @Test
+  public void differentPathsWithSameElementsEqual() throws Exception {
+    Path aPath = new Path(createVertex("1"));
+    Path anotherPath = new Path(createVertex("1"));
+    assertThat(aPath, is(anotherPath));
+  }
+
+  @Test
+  public void pathsWithManyIdenticalElementsEqual() throws Exception {
+    Path aPath = new Path(createVertex("1"), createVertex("2"), createVertex("3"));
+    Path anotherPath = new Path(createVertex("1"), createVertex("2"), createVertex("3"));
+    assertThat(aPath, is(anotherPath));
+  }
+
+  @Test
+  public void equalPathsHaveIdenticalHashCodes() throws Exception {
+    Path aPath = new Path(createVertex("1"), createVertex("2"), createVertex("3"));
+    Path anotherPath = new Path(createVertex("1"), createVertex("2"), createVertex("3"));
+    assertThat(aPath.hashCode(), is(anotherPath.hashCode()));
+  }
+
+  private Vertex createVertex(String id) {
+    return new Vertex(id, id);
+  }
+}
