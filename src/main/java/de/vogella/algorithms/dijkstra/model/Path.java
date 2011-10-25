16038dcbfd18c6c73e562d59a27fb5e410c56df8
Urs Reupke <ursreupke@gmail.com>
2011-10-25 17:53:20 +0200

    Added commons lang, sourcefolders

---
+++ src/main/java/de/vogella/algorithms/dijkstra/model/Path.java
@@ -0,0 +1,12 @@
+package de.vogella.algorithms.dijkstra.model;
+
+import java.util.List;
+
+public class Path {
+
+  private List<Vertex> nodes;
+
+  public Path(List<Vertex> nodes) {
+    this.nodes = nodes;
+  }
+}
